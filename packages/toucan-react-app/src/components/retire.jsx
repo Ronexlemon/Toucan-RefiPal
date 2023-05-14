190ec7592057cbd31c42dc09ccddd86b0fcbe18d
RonexLemon <ronexondimu@gmail.com>
2023-05-14 17:46:50 +0300

    retire btc

--- packages/toucan-react-app/src/components/retire.jsx
+++ packages/toucan-react-app/src/components/retire.jsx
@@ -1,11 +1,16 @@
-import React from "react"
-//const ToucanClient = require("toucan-sdk");
-import { useProvider, useSigner } from "wagmi";
 
+
+import React from "react"
+import { useContract } from "wagmi"
+import { useSigner } from "wagmi"
+import { useContractWrite } from "wagmi"
+import { ToucanCeloContractAddress } from "../Addresses/address"
+import ToucanAbi from "../ABI/Toucan.json";
+import Ierc20Abi from "../ABI/Ierc20.json";
+import { TCo2Address } from "../Addresses/address"
+import { ethers } from "ethers"
 import { useState,useEffect } from "react"
 import { FaSpinner } from 'react-icons/fa';
-
-import { ethers } from "ethers";
 
 
 export default function RetireBTC() {
@@ -14,7 +19,7 @@ export default function RetireBTC() {
   
   //   const toucan = new ToucanClient("alfajores", provider);
   //   signer && toucan.setSigner(signer);
-  //   const [amount, setAmount] = useState();
+     const [amount, setAmount] = useState();
   //   // we will store our return value here
   // const [tco2address, setTco2address] = useState("");
 
@@ -29,12 +34,75 @@ export default function RetireBTC() {
     
      
       
-      const retireBtcForTco2 = async () => {
+      // const retireBtcForTco2 = async () => {
+      //   try {
+      //     if (amount == undefined) {
+      //       alert("Please enter the amount");
+      //     } else {
+      //      redeemPoolToken();
+      //     }
+      //   } catch (e) {
+      //     console.log("the error to approve is", e);
+      //   }
+      // };
+      // console.log("the amount",typeof(amount));
+      // useEffect(()=>{
+
+      // },[amount,appr,deposit])
+      
+    const [isLoading, setIsLoading] = useState(false);
+    const {
+        
+        writeAsync: deposit
+        
+      } = useContractWrite({
+        address:ToucanCeloContractAddress,
+        abi:ToucanAbi,
+        functionName: "depositTCo2",
+        args:[TCo2Address,ethers.utils.parseEther(amount || "0")]
+      })
+      const {
+        
+        writeAsync: appr
+        
+      } = useContractWrite({
+        address:TCo2Address,
+        abi:Ierc20Abi,
+        functionName: "approve",
+        args:[ToucanCeloContractAddress,ethers.utils.parseEther(amount || "0")]
+      })
+      const {
+        
+        writeAsync: allow
+        
+      } = useContractWrite({
+        address:TCo2Address,
+        abi:Ierc20Abi,
+        functionName: "allowance",
+        args:[ToucanCeloContractAddress,ethers.utils.parseEther("1")]
+      })
+      const approve = async () => {
+        try {
+          setIsLoading(true); // set isLoading to true before the async call
+          await appr();
+        } catch (e) {
+          console.log("the error to approve is", e);
+        } finally {
+          setIsLoading(false); // set isLoading to false after the async call
+        }
+      };
+      
+      const depositBtc = async () => {
         try {
           if (amount == undefined) {
             alert("Please enter the amount");
           } else {
-           redeemPoolToken();
+            setIsLoading(true); // set isLoading to true before the async calls
+            await approve();
+            setTimeout(async () => {
+              await deposit();
+              setIsLoading(false); // set isLoading to false after the async calls
+            }, 10000);
           }
         } catch (e) {
           console.log("the error to approve is", e);
@@ -55,15 +123,11 @@ export default function RetireBTC() {
           </div>
           <div className="flex justify-around items-center mt-14">
             <button className="rounded bg-orange-100 w-20">Close</button>
-            <button className="rounded bg-orange-100 w-20" onClick={() => {retireBtcForTco2()}}>
+            <button className="rounded bg-orange-100 w-20" onClick={()=>(depositBtc())} >
               Retire
             </button>
           </div>
-          {isLoading && ( // add this conditional rendering for the loading icon
-            <div className="flex justify-center items-center">
-              <FaSpinner className="animate-spin h-8 w-8 text-green-500" />
-            </div>
-          )}
+         
         </div>
       );
   }
